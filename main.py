c8e9241e2c886251c6306c6fc452d5a0d5009799
Donatas Noreika <donoras@gmail.com>
2022-03-08 09:43:17 +0200

    Pridėtas naujo straipsnio pridėjimas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ straipsniai = [{
     'pavadinimas': 'Apie nieką',
     'tekstas': 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.',
     'status': 'published'
-},
+    },
     {
         'data': '2020 02 01',
         'autorius': 'KITAS AUTORIUS',
@@ -44,6 +44,25 @@ def index():
 def article(title):
     return render_template('article.html', title=title, straipsniai=straipsniai)
 
+
+@app.route('/add_article', methods=['GET', 'POST'])
+def add_article():
+    if request.method == "POST":
+        autorius = request.form['autorius']
+        pavadinimas = request.form['pavadinimas']
+        date = request.form['date']
+        tekstas = request.form['tekstas']
+        straipsniai.append(
+        {
+            'data': date,
+            'autorius': autorius,
+            'pavadinimas': pavadinimas,
+            'tekstas': tekstas,
+            'status': 'published'
+        }
+        )
+        return redirect(url_for('index'))
+    return render_template('add_article.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
