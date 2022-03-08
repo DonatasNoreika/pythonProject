fb8620e3e3297dcd65dbb78757c1122541adcc2b
Donatas Noreika <donoras@gmail.com>
2022-03-08 10:03:49 +0200

    Pridėta contact_us.html

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect
+from forms import ContactForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfgsfdgsdfgsdfgsdf'
 
 straipsniai = [{
     'data': '2020 01 01',
@@ -63,6 +65,13 @@ def add_article():
         )
         return redirect(url_for('index'))
     return render_template('add_article.html')
+
+@app.route('/contact_us', methods=['GET', 'POST'])
+def contact_us():
+    form = ContactForm()
+    if form.validate_on_submit():
+        return render_template('contact_success.html', form=form)
+    return render_template('contact_us.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
